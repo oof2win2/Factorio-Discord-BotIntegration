730de9e9b262cb20d93801e023162eeada6f804f
oof2win2 <honza.koco44@gmail.com>
2021-06-16 18:49:10 +0200

    :sparkles: Fixed for new version of Jammy

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ This is a mod that allows the Discord [AwF Bot](https://github.com/James-Hackett
 - Log when a player dies
 - Allow integration with [AwF Bot](https://github.com/James-Hackett/AwF-Bot) (show deaths, research in Discord)
 - Is developed by the same people as the bot, which allows better integration
+
+The log file is in `script-output/ext/awflogging.out` due to the fact that it is used on [AwF](http://awf.yt) this exact way. 
--- scripts/logging.lua
+++ scripts/logging.lua
@@ -1,19 +1,63 @@
-local function on_rocket_launched(event)
-	print ("JLOGGER: ROCKET: " .. "ROCKET LAUNCHED")
-end
-
-local function on_pre_player_died(event)
-	if event.cause and event.cause.type == "character" then --PvP death
-		print("JLOGGER: DIED: PLAYER: " .. game.get_player(event.player_index).name .. " " .. (game.get_player(event.cause.player.index).name or "no-cause"))
-	elseif (event.cause) then
-		print ("JLOGGER: DIED: " .. game.get_player(event.player_index).name .. " " .. (event.cause.name or "no-cause"))
+local function on_pre_player_died (e)
+	if e.cause and e.cause.type == "character" then --PvP death
+		print("JLOGGER: DIED: PLAYER: " .. game.get_player(e.player_index).name .. " " .. (game.get_player(e.cause.player.index).name or "no-cause"))
+	elseif (e.cause) then
+		print ("JLOGGER: DIED: " .. game.get_player(e.player_index).name .. " " .. (e.cause.name or "no-cause"))
 	else
-		print ("JLOGGER: DIED: " .. game.get_player(event.player_index).name .. " " .. "no-cause") --e.g. poison damage
+		print ("JLOGGER: DIED: " .. game.get_player(e.player_index).name .. " " .. "no-cause") --e.g. poison damage
 	end
 end
 
-local function get_infinite_research_name(name) --gets the name of infinite research (without numbers)
-  return string.match(name, "^(.-)%-%d+$") or name
+-- Determines and logs a leave reason for a player leaving, logs it to script-output/ext/awflogging.out
+local function on_player_left_game(e)
+	local player = game.get_player(e.player_index)
+	local reason
+	if e.reason == defines.disconnect_reason.quit then
+		reason = "quit"
+	elseif e.reason == defines.disconnect_reason.dropped then
+		reason = "dropped"
+	elseif e.reason == defines.disconnect_reason.reconnect then
+		reason = "reconnect"
+	elseif e.reason == defines.disconnect_reason.wrong_input then
+		reason = "wrong_input"
+	elseif e.reason == defines.disconnect_reason.desync_limit_reached then
+		reason = "desync_limit_reached"
+	elseif e.reason == defines.disconnect_reason.cannot_keep_up then
+		reason = "cannot_keep_up"
+	elseif e.reason == defines.disconnect_reason.afk then
+		reason = "afk"
+	elseif e.reason == defines.disconnect_reason.kicked then
+		reason = "kicked"
+	elseif e.reason == defines.disconnect_reason.kicked_and_deleted then
+		reason = "kicked_and_deleted"
+	elseif e.reason == defines.disconnect_reason.banned then
+		reason = "banned"
+	elseif e.reason == defines.disconnect_reason.switching_servers then
+		reason = "switching_servers"
+	else
+		reason = "other"
+	end
+	game.write_file("ext/awflogging.out", game.table_to_json(
+		{
+			type='leave',
+			playerName=player.name,
+			reason=reason
+		} .. "\n"
+	), true, 0)
+end
+local function on_player_joined_game(e)
+	local player = game.get_player(e.player_index)
+	game.write_file("ext/awflogging.out", game.table_to_json(
+		{
+			type='join',
+			playerName=player.name
+		} .. "\n"
+	), true, 0)
+end
+
+local function get_infinite_research_name(name)
+	-- gets the name of infinite research (without numbers)
+  	return string.match(name, "^(.-)%-%d+$") or name
 end
 
 local function on_research_finished(event)
@@ -21,93 +65,71 @@ local function on_research_finished(event)
 	print ("JLOGGER: RESEARCH FINISHED: " .. research_name .. " " .. (event.research.level or "no-level"))
 end
 
-local function on_trigger_fired_artillery(event)
-	print ("JLOGGER: ARTILLERY: " .. event.entity.name .. (event.source.name or "no source"))
-end
-
 local function on_built_entity(event)
-		if global[event.player_index] == nil then
-				-- format of array: {entities placed, ticks played}
-                global[event.player_index] = {1, 0}
-		else
-			local toWrite = global[event.player_index]
-			toWrite[1] = toWrite[1] + 1 --indexes start with 1 in lua
-            global[event.player_index] = toWrite
-        end
-end
-
-local function printPlayerBuilds()
-        -- prints player name and player online time
-        for _, p in pairs(game.players)
-        do
-				if global[p.index] ~= nil
-				then -- ~= is lua's != operator
-						print ("JLOGGER: BUILT ENTITY: " .. p.name .. " " .. global[p.index][1])
-						global[p.index][1] = 0
-                end
-        end
-end
-
-local function logPlayerTime()
-	for _, p in pairs(game.players)
-	do
-		if global[p.index] == nil then
-				-- format of array: {entities placed, ticks played}
-				global[p.index] = {0, p.online_time}
-				print ("JLOGGER: TIME PLAYED: " .. p.name .. " " .. p.online_time)
-		else
-			local playerStats = global[p.index]
-			print ("JLOGGER: TIME PLAYED: " .. p.name .. " " .. (p.online_time - playerStats[2]))
-			playerStats[2] = p.online_time --set it back to the time played (currently)
-			global[p.index] = playerStats
-		end
+	-- get the corresponding data
+	local player = game.get_player(event.player_index)
+	local data = global.playerstats[player.name]
+	if data == nil then
+		-- format of array: {entities placed, ticks played}
+		global.playerstats[player.name] = {1, 0}
+	else
+		data[1] = data[1] + 1 --indexes start with 1 in lua
+		global.playerstats[player.name] = data
 	end
+end
+
+local function on_init ()
+	global.playerstats = {}
 end
 
 local function logStats()
+	-- log built entities and playtime of players
 	for _, p in pairs(game.players)
 	do
-		if (global[p.index] == nil) then
+		local pdat = global.playerstats[p.name]
+		if (pdat == nil) then
 				-- format of array: {entities placed, ticks played}
-				global[p.index] = {0, p.online_time}
+				pdat = {0, p.online_time}
 				print ("JLOGGER: STATS: " .. p.name .. " " .. 0 .. " " .. p.online_time)
+				global.playerstats[p.name] = pdat
 		else
-			local playerStats = global[p.index]
-			if (playerStats[1] ~= 0 or (p.online_time - playerStats[2]) ~= 0) then
-				print ("JLOGGER: STATS: " .. p.name .. " " .. playerStats[1] .. " " .. (p.online_time - playerStats[2]))
+			if (pdat[1] ~= 0 or (p.online_time - pdat[2]) ~= 0) then
+				print ("JLOGGER: STATS: " .. p.name .. " " .. pdat[1] .. " " .. (p.online_time - pdat[2]))
 			end
-			playerStats[2] = p.online_time --set it back to the time played (currently)
-			playerStats[1] = 0 --reset the number of built entities
-			global[p.index] = playerStats
+			-- update the data
+			global.playerstats[p.name] = {0, p.online_time}
 		end
 	end
 end
 
-local lib = {}
-
-lib.events =
-{
-	[defines.events.on_rocket_launched] = on_rocket_launched,
-	[defines.events.on_pre_player_died] = on_pre_player_died,
-	[defines.events.on_research_finished] = on_research_finished,
-	[defines.events.on_trigger_fired_artillery] = on_trigger_fired_artillery,
-	[defines.events.on_built_entity] = on_built_entity
-}
-
-lib.on_nth_tick = {
-	-- 60 ticks/s (same as UPS)
-	-- 60s * 60t * 15m = logging every 15 minutes
-	[60*60*15] = function()
-		-- logPlayerTime()
-		-- printPlayerBuilds()
-		logStats()
-	end
-}
-
-lib.on_event = function(event)
-  local action = events[event.name]
-  if not action then return end
-  return action(event)
+local function on_rocket_launched(e)
+	print ("JLOGGER: ROCKET: " .. "ROCKET LAUNCHED")
+end
+local function checkEvolution(e)
+	print("JLOGGER: EVOLUTION: " .. string.format("%.4f", game.forces["enemy"].evolution_factor))
+end
+local function on_trigger_fired_artillery(e)
+	print ("JLOGGER: ARTILLERY: " .. e.entity.name .. (e.source.name or "no source"))
 end
 
-return lib
+local logging = {}
+logging.events = {
+	[defines.events.on_rocket_launched] = on_rocket_launched,
+	[defines.events.on_research_finished] = on_research_finished,
+	[defines.events.on_player_joined_game] = on_player_joined_game,
+	[defines.events.on_player_left_game] = on_player_left_game,
+	[defines.events.on_pre_player_died] = on_pre_player_died,
+	[defines.events.on_built_entity] = on_built_entity,
+	[defines.events.on_trigger_fired_artillery] = on_trigger_fired_artillery,
+}
+
+logging.on_nth_tick = {
+	[60*60*15] = function() -- every 15 minutes
+		logStats()
+	end,
+	[60*60] = checkEvolution,
+}
+
+logging.on_init = on_init
+
+return logging
